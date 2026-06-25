2342b52ed4cc864819bc7bdd752525ab2c199037
Vitor Madeira <vitorsoutom@hotmail.com>
2026-06-25 09:01:06 -0300

    style: change background color of the main container for better aesthetics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Sidebar } from "./_components/sidebar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-[#0F111A]">
+    <div className="flex min-h-screen bg-[#0B1326]">
       <Sidebar />
     </div>
   );
