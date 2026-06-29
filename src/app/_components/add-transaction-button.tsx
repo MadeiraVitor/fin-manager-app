3599623dd9c27cfae689cda55b947542c9f54e99
Vitor Madeira <vitorsoutom@hotmail.com>
2026-06-29 19:10:46 -0300

    feat: enhance AddTransactionButton with form validation and dynamic select options

--- src/app/_components/add-transaction-button.tsx
+++ src/app/_components/add-transaction-button.tsx
@@ -16,10 +16,61 @@ import {
 } from "@/src/app/_components/ui/dialog";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
-import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  TRANSACTION_CATEGORY_OPTIONS,
+  TRANSACTION_PAYMENT_METHOD_OPTIONS,
+  TRANSACTION_TYPE_OPTIONS,
+} from "../_constants/transaction";
+
+import {
+  createTransactionFormSchema,
+  type CreateTransactionFormData,
+} from "../_schemas/transaction";
+import { addTransaction } from "../_actions/add-transaction";
 
 export const AddTransactionButton = () => {
   const [open, setIsOpen] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<CreateTransactionFormData>({
+    resolver: zodResolver(createTransactionFormSchema),
+    defaultValues: {
+      name: "" as any,
+      amount: "" as any,
+      type: "" as any,
+      category: "" as any,
+      paymentMethod: "" as any,
+      date: "" as any,
+    },
+    mode: "onBlur",
+  });
+
+  const onSubmit = async (data: CreateTransactionFormData) => {
+    try {
+      console.log(data);
+      await addTransaction(data);
+      reset();
+      setIsOpen(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <section>
@@ -36,64 +87,158 @@ export const AddTransactionButton = () => {
 
         <DialogContent className="bg-[#182132]">
           <DialogHeader className="p-2 border-b border-white/20">
-            <DialogTitle className="font-semibold text-lg">Nova transação</DialogTitle>
+            <DialogTitle className="font-semibold text-lg">
+              Nova transação
+            </DialogTitle>
           </DialogHeader>
 
-          <form className="flex flex-col gap-4 pt-4">
+          <form
+            className="flex flex-col gap-4 pt-4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <div className="space-y-2">
-              <Label className="font-medium text-sm text-[#CAD5E2]">Título</Label>
-              <Input id="name" placeholder="Ex: Almoço, Freela..." />
+              <Label className="font-medium text-sm text-[#CAD5E2]">
+                Título
+              </Label>
+              <Input
+                id="name"
+                placeholder="Ex: Almoço, Freela..."
+                {...register("name")}
+              />
+
+              {errors.name && (
+                <p className="text-red-500 text-xs">{errors.name.message}</p>
+              )}
             </div>
             <div className="space-y-2">
-              <Label className="font-medium text-sm text-[#CAD5E2]">Valor (R$)</Label>
-              <Input id="amount" type="number" placeholder="0,00" />
+              <Label className="font-medium text-sm text-[#CAD5E2]">
+                Valor (R$)
+              </Label>
+              <Input
+                id="amount"
+                type="number"
+                placeholder="0,00"
+                {...register("amount", { valueAsNumber: true })}
+              />
+              {errors.amount && (
+                <p className="text-red-500 text-xs">{errors.amount.message}</p>
+              )}
             </div>
             <div className="space-y-2">
               <Label className="font-medium text-sm text-[#CAD5E2]">Tipo</Label>
-              <Select>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Selecione o tipo" />
-                </SelectTrigger>
 
-                <SelectContent>
-                  
-                </SelectContent>
-              </Select>
+              <Controller
+                control={control}
+                name="type"
+                render={({ field }) => (
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Selecione o tipo" />
+                    </SelectTrigger>
+
+                    <SelectContent>
+                      {TRANSACTION_TYPE_OPTIONS.map((opt) => (
+                        <SelectItem key={opt.value} value={opt.value}>
+                          {opt.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+
+              {errors.type && (
+                <p className="text-red-500 text-xs">{errors.type.message}</p>
+              )}
             </div>
             <div className="space-y-2">
-              <Label className="font-medium text-sm text-[#CAD5E2]">Categoria</Label>
-              <Select>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Selecione o tipo" />
-                </SelectTrigger>
+              <Label className="font-medium text-sm text-[#CAD5E2]">
+                Categoria
+              </Label>
 
-                <SelectContent>
-                  
-                </SelectContent>
-              </Select>
+              <Controller
+                control={control}
+                name="category"
+                render={({ field }) => (
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Selecione a categoria" />
+                    </SelectTrigger>
+
+                    <SelectContent>
+                      {TRANSACTION_CATEGORY_OPTIONS.map((opt) => (
+                        <SelectItem key={opt.value} value={opt.value}>
+                          {opt.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+
+              {errors.category && (
+                <p className="text-red-500 text-xs">
+                  {errors.category.message}
+                </p>
+              )}
             </div>
             <div className="space-y-2">
-              <Label className="font-medium text-sm text-[#CAD5E2]">Método de pagamento</Label>
-              <Select>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Selecione o tipo" />
-                </SelectTrigger>
+              <Label className="font-medium text-sm text-[#CAD5E2]">
+                Método de pagamento
+              </Label>
 
-                <SelectContent>
-                  
-                </SelectContent>
-              </Select>
+              <Controller
+                control={control}
+                name="paymentMethod"
+                render={({ field }) => (
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Selecione o método de pagamento" />
+                    </SelectTrigger>
+
+                    <SelectContent>
+                      {TRANSACTION_PAYMENT_METHOD_OPTIONS.map((opt) => (
+                        <SelectItem key={opt.value} value={opt.value}>
+                          {opt.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+
+              {errors.paymentMethod && (
+                <p className="text-red-500 text-xs">
+                  {errors.paymentMethod.message}
+                </p>
+              )}
             </div>
             <div className="space-y-2">
               <Label className="font-medium text-sm text-[#CAD5E2]">Data</Label>
-              <Input id="date" placeholder="__/__/____" />
+              <Input
+                type="date"
+                id="date"
+                placeholder="__/__/____"
+                {...register("date")}
+              />
+              {errors.date && (
+                <p className="text-red-500 text-xs">{errors.date.message}</p>
+              )}
             </div>
 
             <DialogFooter className="gap-4 border-none">
-              <button className="border border-[#CAD5E2] rounded-lg w-1/3 py-2.5 cursor-pointer">Cancelar</button>
-              <button className="bg-[#10B981] w-2/3 flex items-center justify-center gap-2 rounded-lg cursor-pointer">
+              <button className="border border-[#CAD5E2] rounded-lg w-1/3 py-2.5 cursor-pointer">
+                Cancelar
+              </button>
+              <button
+                type="submit"
+                className="bg-[#10B981] w-2/3 flex items-center justify-center gap-2 rounded-lg cursor-pointer"
+                disabled={isSubmitting}
+              >
                 <Image src={confirmIcon} alt="Confirm Icon" />
-                <p className="font-semibold text-sm">Salvar transação</p>
+                <p className="font-semibold text-sm">
+                  {isSubmitting ? "Salvando..." : "Salvar transação"}
+                </p>
               </button>
             </DialogFooter>
           </form>
